a228400015b627c9661ddc816715d1875af404e9
Guy Hazan <guy.hazan@driivz.com>
2021-10-21 18:02:41 +0300

    Initial commit

---
+++ src/main/java/com/example/yofeely/YofeelyApplication.java
@@ -0,0 +1,13 @@
+package com.example.yofeely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YofeelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YofeelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yofeely/YofeelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yofeely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YofeelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
